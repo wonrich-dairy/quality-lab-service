00bc4f241218a3c5d8292602047cc39d91377ca5
Mitheja-Manoharie <mmanoharie@gmail.com>
2026-09-18 22:17:57 +0530

    SCRUM-108: Add MySQL health check to /health endpoint

--- src/QualityLab.Api/Program.cs
+++ src/QualityLab.Api/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddHealthChecks(); 
+builder.Services.AddHealthChecks()
+    .AddMySql(builder.Configuration.GetConnectionString("QualityLabDb")!);
 
 var app = builder.Build();
 
