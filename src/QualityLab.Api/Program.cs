c8b4479719d3c28f4f2dc57e4fa448924ecb643a
Mitheja-Manoharie <mmanoharie@gmail.com>
2026-09-21 14:41:54 +0530

    SCRUM-108: Remove HTTPS redirection (container serves HTTP; TLS terminates at App Service)

--- src/QualityLab.Api/Program.cs
+++ src/QualityLab.Api/Program.cs
@@ -15,8 +15,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
